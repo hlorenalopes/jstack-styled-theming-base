d6f9e46424212c4ea07021c8324232f0780a18f7
hlorenalopes <hlorenalopes@gmail.com>
2022-05-26 16:10:10 -0600

    Trabalhando com states

--- src/App.js
+++ src/App.js
@@ -6,23 +6,52 @@ import Layout from './components/Layout';
 
 import themes from './styles/themes';
 
-function App() {
-  const [theme, setTheme] = useState('dark');
+class App extends React.Component {
+  constructor(props) {
+    // primeira forma de usar states com class components
+    super(props); // executa o método construtor
 
-  const currentTheme = useMemo(() => {
-    return themes[theme] || themes.dark;
-  }, [theme]);
-
-  function handleToggleTheme() {
-    setTheme((prevState) => (prevState === 'dark' ? 'light' : 'dark'));
+    this.state = {
+      theme: 'dark',
+    };
   }
 
-  return (
-    <ThemeProvider theme={currentTheme}>
-      <GlobalStyle />
-      <Layout onToggleTheme={handleToggleTheme} selectedTheme={theme} />
-    </ThemeProvider>
-  );
+  render() {
+    const { theme } = this.state;
+
+    return (
+      <ThemeProvider theme={themes[theme] || themes.dark}>
+        <GlobalStyle />
+        <Layout
+          onToggleTheme={() => {
+            this.setState((prevState) => ({
+              theme: prevState.theme === 'dark' ? 'light' : 'dark',
+            })); //faz um merge com o this.state passado no constructor
+          }}
+          selectedTheme={theme}
+        />
+      </ThemeProvider>
+    );
+  }
 }
+
+// function App() {
+//   const [theme, setTheme] = useState('dark');
+
+//   const currentTheme = useMemo(() => {
+//     return themes[theme] || themes.dark;
+//   }, [theme]);
+
+//   function handleToggleTheme() {
+//     setTheme((prevState) => (prevState === 'dark' ? 'light' : 'dark'));
+//   }
+
+// return (
+//   <ThemeProvider theme={currentTheme}>
+//     <GlobalStyle />
+//     <Layout onToggleTheme={handleToggleTheme} selectedTheme={theme} />
+//   </ThemeProvider>
+// );
+// }
 
 export default App;
