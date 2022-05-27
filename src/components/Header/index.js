8040cdb73c3b9647b890025676046f037213dacd
hlorenalopes <hlorenalopes@gmail.com>
2022-05-27 15:52:23 -0600

    Consumindo a Context API

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+
+import { ThemeContext } from '../../contexts/ThemeContext';
 
 import { Container } from './styles';
 
 export default class Header extends Component {
-  static propTypes = {
-    //com static o atributo pertence à classe em si, em vez da instância da classe sem o static
-    selectedTheme: PropTypes.string.isRequired,
-    onToggleTheme: PropTypes.func.isRequired,
-  };
-
   render() {
-    const { onToggleTheme, selectedTheme } = this.props;
-
     return (
-      <Container>
-        <h1>JStack's Blog</h1>
-        <button type='button' onClick={onToggleTheme}>
-          {selectedTheme === 'dark' ? '🌞' : '🌛'}
-        </button>
-      </Container>
+      <ThemeContext.Consumer>
+        {(value) => (
+          <Container>
+            <h1>JStack's Blog</h1>
+            <button type='button' onClick={value.handleToggleTheme}>
+              {value.theme === 'dark' ? '🌞' : '🌛'}
+            </button>
+          </Container>
+        )}
+      </ThemeContext.Consumer>
     );
   }
 }
