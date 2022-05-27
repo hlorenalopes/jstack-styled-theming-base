1b2a68f98bbf0b27e7d0c23ffad1c5a0c91fa1b4
hlorenalopes <hlorenalopes@gmail.com>
2022-05-27 14:29:13 -0600

    Refatorando para class properties

--- src/App.js
+++ src/App.js
@@ -7,22 +7,17 @@ import Layout from './components/Layout';
 import themes from './styles/themes';
 
 class App extends React.Component {
-  constructor(props) {
-    // primeira forma de usar states com class components
-    super(props); // executa o método construtor
+  state = {
+    //cria uma propriedade na classe e atribui um valor pra ela
+    theme: 'dark',
+  };
 
-    this.state = {
-      theme: 'dark',
-    };
-
-    this.handleToggleTheme = this.handleToggleTheme.bind(this); //tá fazendo com que o this do handleToggleTeam seja o this do construtor que é a classe App
-  }
-
-  handleToggleTheme() {
+  handleToggleTheme = () => {
+    // quando usa arrow function tá herdando o objeto this de onde a função foi criada, então não precisa do bind
     this.setState((prevState) => ({
       theme: prevState.theme === 'dark' ? 'light' : 'dark',
     }));
-  }
+  };
 
   render() {
     const { theme } = this.state;
