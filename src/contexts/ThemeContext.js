c8c05a5ada66f64bc37db5422c0274c83f7f35c0
hlorenalopes <hlorenalopes@gmail.com>
2022-05-30 15:52:11 -0600

    Salvando o theme no localStorage e pegando o valor do localStorage - não é a melhor forma

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -4,13 +4,18 @@ export const ThemeContext = createContext();
 
 export class ThemeProvider extends React.Component {
   state = {
-    theme: 'dark',
+    theme: JSON.parse(localStorage.getItem('theme')), //mantém o estado se atualizar a página, mas aqui não é o melhor lugar pois pode dar erro
   };
 
   handleToggleTheme = () => {
-    this.setState((prevState) => ({
-      theme: prevState.theme === 'dark' ? 'light' : 'dark',
-    }));
+    this.setState(
+      (prevState) => ({
+        theme: prevState.theme === 'dark' ? 'light' : 'dark',
+      }),
+      () => {
+        localStorage.setItem('theme', JSON.stringify(this.state.theme));
+      },
+    );
   };
 
   render() {
