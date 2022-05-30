5e8aeb01dbc7c268858e245c211804e0eeee485d
hlorenalopes <hlorenalopes@gmail.com>
2022-05-30 15:05:44 -0600

    componentDidCatch

--- src/App.js
+++ src/App.js
@@ -24,6 +24,10 @@ class App extends React.Component {
     });
   }
 
+  componentDidCatch(error, info) {
+    console.log('componentDidCatch', { error, info });
+  }
+
   render() {
     console.log('rendered');
 
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -7,6 +7,7 @@ import Footer from '../Footer';
 export default function Layout() {
   return (
     <>
+      {undefined.map((item) => item)}
       <Header />
       <PostsList />
       <Footer />
