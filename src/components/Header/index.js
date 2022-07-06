c9c4dea5dea12f205316bc21eafdd3c9ee453e01
hlorenalopes <hlorenalopes@gmail.com>
2022-07-06 16:06:51 -0600

    Navegação programática - functional component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 import { Container } from './styles';
 
 export default function Header({ onToggleTheme, selectedTheme }) {
+  const history = useHistory();
+
+  function handleNavigate() {
+    history.push('/posts');
+  }
+
   return (
     <Container>
       <h1>JStack's Blog</h1>
       <button type='button' onClick={onToggleTheme}>
         {selectedTheme === 'dark' ? '🌞' : '🌛'}
+      </button>
+      <button onClick={handleNavigate} style={{ color: '#fff' }}>
+        Navegar
       </button>
     </Container>
   );
--- src/pages/Post/index.js
+++ src/pages/Post/index.js
@@ -1,28 +1,13 @@
 import React, { useMemo } from 'react';
 import { useParams, useLocation } from 'react-router-dom';
 
-export default class Post extends React.Component {
-  constructor(props) {
-    super(props);
+export default function Post() {
+  const params = useParams();
+  const { search } = useLocation();
+  const queryParams = useMemo(() => new URLSearchParams(search), [search]);
 
-    const { search } = this.props.location;
-    this.queryParams = new URLSearchParams(search);
-  }
+  console.log(params);
+  console.log(queryParams.get('meuQueryParam'));
 
-  render() {
-    console.log(this.queryParams.get('meuQueryParam'));
-
-    return <h1>Post page</h1>;
-  }
+  return <h1>Post page</h1>;
 }
-
-// export default function Post() {
-//   const params = useParams();
-//   const { search } = useLocation();
-//   const queryParams = useMemo(() => new URLSearchParams(search), [search]);
-
-//   console.log(params);
-//   console.log(queryParams.get('meuQueryParam'));
-
-//   return <h1>Post page</h1>;
-// }
